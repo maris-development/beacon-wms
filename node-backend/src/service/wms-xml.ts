b449ef53374006dbc367e60476ac07d57a0772cc
Paul Weerheim <paul@maris.nl>
2025-10-02 13:51:25 +0200

    Added HTTP_HOST env var

--- node-backend/src/service/wms-xml.ts
+++ node-backend/src/service/wms-xml.ts
@@ -21,11 +21,13 @@ export class WmsXmlService {
     }
 
     async getCapabilities(req: Request, res: Response, workspace: WorkspaceConfig, availableStyles: string[], wmsVersion: string = "1.3.0") {
+        const http_host = process.env.HTTP_HOST || req.get('host');
+
         const params = {
             wmsVersion,
             workspace,
             availableStyles,
-            baseUrl: req.protocol + '://' + req.get('host') + req.originalUrl.split('?')[0],
+            baseUrl: req.protocol + '://' + http_host + req.originalUrl.split('?')[0],
             server: await this.config.getServerConfig()
         }
         
